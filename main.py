e1f3f7a878c733a7210af1086775964607d0fcd5
hammer8130 <154071436+hammer8130@users.noreply.github.com>
2025-04-24 13:55:59 +0900

    fix:total_score dict sorting / router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import resumes, postings, agent_router
+from routers import agent, resumes, postings
 from exception.handlers import register_exception_handlers
 
 app = FastAPI()
@@ -19,7 +19,7 @@ app.add_middleware(
 # 라우터 등록
 app.include_router(resumes.router, prefix="/resumes", tags=["Resume"])
 app.include_router(postings.router, prefix="/postings", tags=["Posting"])
-app.include_router(agent_router.router, prefix="/agent", tags=["Agent"])
+app.include_router(agent.router, prefix="/agent", tags=["Agent"])
 
 #테스트
 # app.include_router(agent_router.router, prefix="/agent-test", tags=["AgentTest"])
